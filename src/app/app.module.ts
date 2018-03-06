c2e3df8a0913cd52fa7de430f063b6e646248dd2
Brandon Chaves <brandonchaves@Pernixs-Mac-mini.local>
2018-03-06 14:09:45 -0600

    Adding component to add invoice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AddInvoiceComponent } from './add-invoice/add-invoice.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddInvoiceComponent
   ],
   imports: [
     BrowserModule
