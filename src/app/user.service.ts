9d15117cf343a2d325a7ad98a4c1a0ac92f5fe69
Alfre97 <alfredo1997avila@gmail.com>
2018-03-13 15:18:43 -0600

    User.service.ts  modified

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,16 +1,31 @@
+import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { catchError, map, tap } from 'rxjs/operators';
+import { of } from 'rxjs/observable/of';
 
-private AddEmitterURL = '/addEmitter';
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 const rootUrl='http://localhost:8080';
+@Injectable()
+export class UserService {
 
-constructor(
-  private http: HttpClient,
-  private messageService: MessageService) { }
+  constructor(private http: HttpClient) {
+  }
 
-  private log(message: string) {
-  this.messageService.add('HeroService: ' + message);
-}
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
 
-AddEmitter(emitter: Emitter): Observable<Emitter> {
-  return this.http.get<Emitter[]>(rootUrl + this.AddEmitterURL);
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 }
